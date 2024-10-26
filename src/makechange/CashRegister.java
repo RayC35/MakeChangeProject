41297d506957ce00c12c5cdb889ba6435798d16f
RaymondCostales <raymond.w.costales@gmail.com>
2024-10-25 18:04:59 -0600

    test, user story 1 & 2

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -6,6 +6,15 @@ public class CashRegister {
 	
 	public static void main(String[]args) {
 		Scanner kb = new Scanner(System.in);
+		
+		System.out.print("Enter the price of the item: ");
+		double price = kb.nextDouble();
+		
+		System.out.print("How much money was tendered: ");
+		double moneyTendered = kb.nextDouble();
+		
+		
+		
 	}
 
 }
